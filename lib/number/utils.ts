e1781402c201645e009ef0b654a5b3fe8683fd2c
timothycohen <tco.physics@gmail.com>
2022-03-22 19:20:52 -0400

    update getRanIntByOOM

--- lib/number/utils.ts
+++ lib/number/utils.ts
@@ -1,34 +1,26 @@
 export const getRanInt = (min: number, max: number): number => Math.floor(Math.random() * (max - min)) + min;
 
-// get a random integer with equal distribution among specified orders of magnitude
-// 0,0 --> 0; 1,1 --> 0-9; 2 --> 10-99; 0,2 --> 0-99;
-// the problem with regular random number generation is that there is 10x as many 10^3 digits as 10^2 digits, etc.
+// the problem with regular random number generation is that there are 10x as many 10^3 digits as 10^2 digits, etc.
 // so the odds of getting order of magnitude 1 (10 digits) is very small compared to the order of magnitude 15
-// conversely, this approach will show '0' as much as digits 1-9 combined
-// for native 0-99 numbers use the pure random integer approach. for sino, which are very large, use the OOM distribution
+// conversely, separating by OOM will show '0' as much as digits 1-9 combined
+// for numbers to 0-99 use the pure random integer approach. for larger numbers use the OOM distribution
 export const getRanIntByOOM = (OOM1: number, OOM2: number): number => {
   const [minOOM, maxOOM] = [Math.min(OOM1, OOM2), Math.max(OOM1, OOM2)];
+
+  if (maxOOM === 1 && minOOM === 0) return getRanInt(0, 10);
+  if (maxOOM === 1) return getRanInt(1, 10);
+  if (maxOOM === 2 && minOOM === 0) return getRanInt(0, 99);
+  if (maxOOM === 2 && minOOM === 1) return getRanInt(1, 99);
+  if (maxOOM === 2) return getRanInt(10, 99);
+
   const OOM = getRanInt(minOOM, maxOOM + 1);
 
   let answer = 0;
   for (let i = 0; i < OOM; i++) {
-    if (i === OOM - 1 && OOM !== 1) answer += getRanInt(1, 10) * 10 ** i;
+    // the most significant digit should not be 0
+    if (i === OOM - 1) answer += getRanInt(1, 10) * 10 ** i;
     else answer += getRanInt(0, 10) * 10 ** i;
   }
-  return answer;
-};
-
-export const getRandomInteger = (num1: number, num2: number): number => {
-  const [min, max] = num1 < num2 ? [num1, num2] : [num2, num1];
-  const order1 = min.toString().length;
-  const order2 = max.toString().length;
-  const randomOrder = getRanInt(order1, order2 + 1);
-  let answer = 0;
-  for (let i = 0; i < randomOrder; i++) {
-    const dig = i === randomOrder - 1 ? getRanInt(0, +max.toString()[0] + 1) : getRanInt(0, 10);
-    answer += dig * 10 ** i;
-  }
-  console.log('randomInt', answer);
   return answer;
 };
 
@@ -43,7 +35,7 @@ export const getNumAtPos = (num: number, pos: number): number => {
   return int;
 };
 
+export const format = (num: number): string => new Intl.NumberFormat().format(num);
+
 export const camelToHuman = (str: string) =>
   str.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase());
-
-export const format = (num: number): string => new Intl.NumberFormat().format(num);
