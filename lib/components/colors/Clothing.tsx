112e08bc54394deffb057c71ceb91517b539827c
timothycohen <tco.physics@gmail.com>
2022-04-03 20:01:35 -0400

    add pullover

--- lib/components/colors/Clothing.tsx
+++ lib/components/colors/Clothing.tsx
@@ -1,14 +1,21 @@
+import Pullover from './Pullover';
 import Skirt from './Skirt';
 
+// others
+// https://freesvg.org/vector-line-art-drawing-of-a-simple-shirt
+// https://freesvg.org/coloured-line-art-vector-image-of-trousers
+
 export default function Clothing() {
+  const colors = [0, 60, 120, 180, 240, 300];
+
   return (
     <>
-      <Skirt color="hsl(0, 100%, 50%)" />
-      <Skirt color="hsl(60, 100%, 50%)" />
-      <Skirt color="hsl(120, 100%, 50%)" />
-      <Skirt color="hsl(180, 100%, 50%)" />
-      <Skirt color="hsl(240, 100%, 50%)" />
-      <Skirt color="hsl(300, 100%, 50%)" />
+      {colors.map(c => (
+        <Skirt key={`skirt-${c}`} color={`hsl(${c}, 100%, 50%)`} />
+      ))}
+      {colors.map(c => (
+        <Pullover key={`pullover-${c}`} color={`hsl(${c}, 100%, 50%)`} />
+      ))}
     </>
   );
 }
---
+++ pages/clothes.tsx
@@ -0,0 +1,8 @@
+import Clothing from 'lib/components/colors/Clothing';
+export default function ClothesPage() {
+  return (
+    <div>
+      <Clothing />
+    </div>
+  );
+}
