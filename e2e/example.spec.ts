e12b77a86b416a998971ebe6b1fe9445a70e898a
timothycohen <tco.physics@gmail.com>
2022-04-16 12:14:47 -0400

    fix example e2e test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -4,5 +4,5 @@ test('should navigate to the numbers page', async ({ page }) => {
   await page.goto('/');
   await page.click('text=Numbers');
   await expect(page).toHaveURL('/numbers');
-  await expect(page.locator('h1')).toContainText('Numbers');
+  await expect(page.locator('h1:has-text("Numbers")')).toContainText('Korean Numbers');
 });
