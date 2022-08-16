65f11d8e74f6b7880dee66e305b1df4e2d39f9a2
timothycohen <contact@timcohen.dev>
2022-08-16 12:10:43 -0400

    remove vestiges

--- process.d.ts
+++ process.d.ts
@@ -1,9 +1,5 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     VITE_APP_URL: string;
-    VITE_SUPABASE_URL: string;
-    VITE_SUPABASE_ANON_KEY: string;
-    SUPABASE_SERVICE_ROLE_KEY: string;
-    CONNECTION_STRING: string;
   }
 }
