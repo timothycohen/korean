b130820704175b4ea216b4011a941ec736321171
Tim <83622086+timothycohen@users.noreply.github.com>
2022-08-20 18:28:39 -0400

    failed like a charm
    e2e failure artifacts are being uploaded successfully

--- e2e/trace.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('e2e tracing', () => {
-  test('this should fail and preserve traces in ci', async ({ page }) => {
-    await page.goto('/');
-    expect(' ').toBe('');
-  });
-});
