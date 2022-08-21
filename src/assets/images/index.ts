bc815849c500fc1224b855e9f12e738d08418881
Tim <83622086+timothycohen@users.noreply.github.com>
2022-08-21 19:15:04 -0400

    srcset for app screenshots (#14)

--- src/assets/images/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as colors } from '$assets/images/colors.png';
-export { default as time } from '$assets/images/time.png';
-export { default as numbers } from '$assets/images/numbers.png';
